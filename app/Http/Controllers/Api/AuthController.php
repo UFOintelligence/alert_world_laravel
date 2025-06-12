9a1a03cd1a3ff6090e35f6a8aaff44ca97e71770
UFOintelligence <ramonbrito10592@gmail.com>
2025-06-11 23:53:38 -0500

    vo

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+
+public function register(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|min:4|confirmed',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
+    }
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    return response()->json([
+        'user' => $user,
+        'token' => $user->createToken('api-token')->plainTextToken,
+    ], 201);
+}
+
+   public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['Las credenciales son incorrectas.'],
+        ]);
+    }
+
+    return response()->json([
+        'user' => $user,
+        'token' => $user->createToken('api-token')->plainTextToken,
+    ]);
+}
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Sesión cerrada correctamente']);
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+}
